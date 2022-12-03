d3e6bed2e3e8fcd0bec370844b29e1eedb881660
Samuel Junior <samuelssj06@gmail.com>
2022-12-03 14:17:33 -0300

    new models

--- receitas/models.py
+++ receitas/models.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 
 class Receita(models.Model):
-    nomde_receita = models.CharField(max_length=200)
-    ingredientes = models.TextField
-    modo_preparo = models.TextField
-    tempo_preparo = models.IntegerField
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
