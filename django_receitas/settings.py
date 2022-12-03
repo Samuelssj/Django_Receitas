e0fdf3de4ffafedbc0b8523ea9708f213d0bd26d
Samuel Junior <samuelssj06@gmail.com>
2022-12-03 13:06:58 -0300

    Using Models

--- django_receitas/settings.py
+++ django_receitas/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_receitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_receita',
+        'USER': 'postgres',
+        'PASSWORD': 'Java',
+        'HOST': 'localhost'
     }
 }
 
