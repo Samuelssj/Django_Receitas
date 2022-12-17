cfe3d72d4161aa0ab448bfe7934286c3d53e4fb7
Samuel Junior <samuelssj06@gmail.com>
2022-12-17 00:59:42 -0300

    new app

--- django_receitas/urls.py
+++ django_receitas/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('receitas.urls')),
     path('admin/', admin.site.urls),
+    path('galeria/', include('galeria.urls')),
 ]
